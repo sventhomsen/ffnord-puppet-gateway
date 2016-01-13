9ec28314acba5e9bc2cb278f8d6921d6ddf38388
Sven Thomsen <sven@tumelum.de>
2016-01-13 19:20:49 +0100

    Added snmpd-restart for nord-gw3

--- files/etc/openvpn/anonvpn-up.sh
+++ files/etc/openvpn/anonvpn-up.sh
@@ -3,4 +3,5 @@ sysctl -w net.ipv4.conf.${1}.rp_filter=0
 ip route replace default via $4 table 42
 ip rule del pref 30000 || true
 ip rule add from $4 lookup 42 pref 30000
+systemctl restart snmpd
 exit 0
